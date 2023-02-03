246b21701f3494abbcc2672ed4190d39ea98850e
varsha Gunjal <varshashingote01@gmail.com>
2023-02-03 12:23:29 +0530

    [varsha] Welcome to Address Book program

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("!!!Welcome To AddressBook Program!!!!");
+                Console.ReadLine();
+
+        }
+    }
+}
